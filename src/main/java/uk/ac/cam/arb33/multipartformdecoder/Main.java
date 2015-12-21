40145d3856a4f31e7e5639a37acfabd312846196
Alastair Beresford <arb33@cam.ac.uk>
2015-12-21 12:42:33 +0000

    Update packagename to better describe project.

--- src/main/java/uk/ac/cam/arb33/multipartformdecoder/Main.java
+++ src/main/java/uk/ac/cam/arb33/multipartformdecoder/Main.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.cam.arb33.multipartformparse;
+package uk.ac.cam.arb33.multipartformdecoder;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
